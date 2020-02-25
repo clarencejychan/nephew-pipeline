e3af703e6be96322a95979fb6e53317bf6bbbe70
Larry Liu <liu.larry96@gmail.com>
2020-02-24 20:53:49 -0500

    Validate Reddit and Twitter query keys

--- utils/validate.go
+++ utils/validate.go
@@ -1,8 +1,4 @@
-package utils
-
-import (
-	"fmt"
-)
+package util
 
 func validateParams(dest string, params map[string]string) bool {
 	switch dest {
@@ -10,15 +6,26 @@ func validateParams(dest string, params map[string]string) bool {
 		return validateReddit(params)
 	case "Twitter":
 		return validateTwitter(params)
+	case "Scheduler":
+		return validateScheduler(params)
 	default:
 		return false
 	}
 }
 
 func validateReddit(params map[string]string) bool {
-
+	_, valid1 := params["after"]
+	_, valid2 := params["before"]
+	_, valid3 := params["subreddit"]
+	return valid1 && valid2 && valid3
 }
 
 func validateTwitter(params map[string]string) bool {
+	_, valid := params["sinceId"]
+	return valid
+}
 
+func validateScheduler(params map[string]string) bool {
+	// add once scheduler is completed
+	return true
 }
